3099d4f4df5850f6b9da514712c14ac05e337620
Lucas Romero <115330219+LucasRomero26@users.noreply.github.com>
2024-12-02 07:12:19 +0000

    modified:   app.py 	new file:   punto1.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
 
+
+st.markdown("<h1 style='text-align: center; color: white;'>Laboratorio Final</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: white;'>Series y Transformada de Fourier</h3>", unsafe_allow_html=True)
+
+
+
+expander = st.expander("Opciones")
+expander.write('''
+    The chart above shows some numbers I picked for you.
+    I rolled actual dice for these, so they're *guaranteed* to
+    be random.
+''')
+expander.image("https://static.streamlit.io/examples/dice.jpg")
